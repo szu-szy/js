9b9fde6e35bcb9ebfc3b7daa68e1ccb236ea566b
szy.szu <szy.szumski@gmail.com>
2024-03-03 11:45:39 +0100

    map method

---
+++ index.js
@@ -0,0 +1,34 @@
+const value = 0;
+const object = {};
+const tab = [];
+
+// nie mozna
+// object = { name: "tekst" };
+// tab = [1, 2, 3];
+
+// mozna
+// sposób na obiekty, dorzucenie wartosci za pomoca properties
+// sposób na tablice > metody tablicowe
+
+// console.log(typeof value);
+// console.log(tab);
+
+// metody tablicowe
+
+// zwracaja tablice
+// map => [] - sluzy do modyfikacji elementow w tablicy
+const items = ["grabie", "szczotka", "grzebien", "durszlak"];
+// tak nie robimy
+const modifiedItems = items.map((item) => {
+  // jezeli item ma wiecej niz 6 znakow to zwroc item-dodatek
+  if (item.length > 6) return `${item}-dodatek`;
+
+  // w innym wypadku zwroc item
+  return item;
+});
+console.log(modifiedItems);
+
+// filter => [] - sluzy do przefiltrowawnia tablicy po warunku
+
+// nic nie zwracaja
+// forEach => undefined
